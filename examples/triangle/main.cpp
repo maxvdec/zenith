ef11ad6c5456ec7f92c1f12bc570be887d57c133
Max Van den Eynde <maxvdec@gmail.com>
2025-07-18 15:13:47 +0200

    Added basic structure to the project

---
+++ examples/triangle/main.cpp
@@ -0,0 +1,15 @@
+/*
+* main.cpp
+* As part of the Zenith project
+* Created by Max Van den Eynde in 2025
+* --------------------------------------
+* Description: A simple example that renders a triangle with Zenith.
+* Copyright (c) 2025 Max Van den Eynde
+*/
+
+#include <zenith/lib.h>
+
+int main() {
+    say_hi();
+    return 0;
+}
